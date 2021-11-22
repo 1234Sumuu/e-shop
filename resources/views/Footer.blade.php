ab5ed3621fd9721f6edf0c4a40e40ff3cd9a0ee2
1234Sumuu <sumaiya.akter.cse@ulab.edu.bd>
2021-11-22 10:54:52 -0800

    laravel e-comm shop

---
+++ resources/views/Footer.blade.php
@@ -0,0 +1,10 @@
+<div style="clear:both" class="panel panel-default">
+    <div class="panel-body">
+      Panel footer
+    </div>
+    <div class="panel-footer">Contact <br>
+                            Email: e-shop@gmaul.com <br>
+                            Phone: +880 1376752 <br>
+                            Home: 22/5 Gulshan, Dhaka-1212 <br>
+                        </div>
+  </div>
